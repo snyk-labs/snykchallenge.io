03ea586c494cdff035fb7cd3c6ebab18375ada18
Micah Silverman <micah@afitnerd.com>
2022-04-08 03:32:24 -0400

    markdown support

---
+++ assets/AboutView.dd14519c.js
@@ -0,0 +1,1 @@
+import{o as s,c,a as n}from"./index.9d0221cd.js";var r=(e,t)=>{const o=e.__vccOpts||e;for(const[_,a]of t)o[_]=a;return o};const i={},u={class:"about"},l=n("h1",null,"This is an about page",-1),d=[l];function v(e,t){return s(),c("div",u,d)}var p=r(i,[["render",v]]);export{p as default};
